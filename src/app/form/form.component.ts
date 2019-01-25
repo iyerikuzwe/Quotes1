b4aced62a50bb2542349af504015f8a9bf03ce02
first-name second-name <email@example.com>
2019-01-25 15:54:05 +0200

    add some changes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  newQuote= new Quote(0,"","","",0,0,new Date());
+  @Output() addQuote=new EventEmitter<Quote>();
+  submitQuote(){
+      this.addQuote.emit(this.newQuote);
+      this.newQuote= new Quote(0,"","","",0,0,new Date());
+  }
   constructor() { }
 
   ngOnInit() {
